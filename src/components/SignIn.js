77f018c166fe628015c6caa80bac143028301712
Mohit <mototurbo12499@gmail.com>
2022-06-11 15:33:26 +0530

    gg

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 import AuthenticationWrapper from "./Authentication/AuthenticationWrapper";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SignIn = () => {
   const [helpDropdown, setHelpDropDown] = useState(false);
